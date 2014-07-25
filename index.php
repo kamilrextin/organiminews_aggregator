135df5d456b9f0cecb010d3a6e539a97c20e2c75
kamilrextin <kamil.rextin@gmail.com>
2014-07-25 16:51:48 -0400

    added GA code

--- index.php
+++ index.php
@@ -10,6 +10,20 @@
 
     <title>Organimi News Aggregator</title>
 
+    <!-- GOOGLE TRACKING CODE -->
+    <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-53186624-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
+<!-- END GOOGLE CODE -->
+
     <!-- Bootstrap core CSS -->
     <link href="assets/css/bootstrap.css" rel="stylesheet">
 
